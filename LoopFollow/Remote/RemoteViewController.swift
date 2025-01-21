7339e51116e52f3adfbe3c015ed83f9f7602ed0e
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2025-01-21 23:20:03 +0100

    Fix switching between remote alternatives

--- LoopFollow/Remote/RemoteViewController.swift
+++ LoopFollow/Remote/RemoteViewController.swift
@@ -33,60 +33,28 @@ class RemoteViewController: UIViewController {
     private func updateView() {
         let remoteType = Storage.shared.remoteType.value
 
+        // Remove existing hosting controller if present
         if let existingHostingController = hostingController {
             existingHostingController.willMove(toParent: nil)
             existingHostingController.view.removeFromSuperview()
             existingHostingController.removeFromParent()
+            hostingController = nil
         }
 
+        // Remove existing child view controllers if present
+        for child in children {
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
+        }
+
+        // Load the new remote type view
         if remoteType == .nightscout {
-                let remoteView = TrioNightscoutRemoteView()
-                hostingController = UIHostingController(rootView: AnyView(remoteView))
-            } else if remoteType == .trc {
-                let trioRemoteControlViewModel = TrioRemoteControlViewModel()
-                let trioRemoteControlView = TrioRemoteControlView(viewModel: trioRemoteControlViewModel)
-                hostingController = UIHostingController(rootView: AnyView(trioRemoteControlView))
-            } else if remoteType == .sms {
-                // Instantiate SMSRemoteViewController from storyboard
-                let storyboard = UIStoryboard(name: "Main", bundle: nil) // Use the storyboard name containing SMSRemoteViewController
-                let smsRemoteVC = storyboard.instantiateViewController(withIdentifier: "SMSRemoteViewController")
-                if let smsVC = smsRemoteVC as? SMSRemoteViewController {
-                    // Successfully instantiated
-                    addChild(smsVC)
-                    view.addSubview(smsVC.view)
-                    smsVC.view.translatesAutoresizingMaskIntoConstraints = false
-                    NSLayoutConstraint.activate([
-                        smsVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                        smsVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                        smsVC.view.topAnchor.constraint(equalTo: view.topAnchor),
-                        smsVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-                    ])
-                    smsVC.didMove(toParent: self)
-                } else {
-                    print("Error: SMSRemoteViewController could not be cast.")
-                    return
-                }
-
-                // Add as child view controller
-                addChild(smsRemoteVC)
-                view.addSubview(smsRemoteVC.view)
-                smsRemoteVC.view.translatesAutoresizingMaskIntoConstraints = false
-                NSLayoutConstraint.activate([
-                    smsRemoteVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                    smsRemoteVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                    smsRemoteVC.view.topAnchor.constraint(equalTo: view.topAnchor),
-                    smsRemoteVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-                ])
-                smsRemoteVC.didMove(toParent: self)
-            } else {
-                hostingController = UIHostingController(rootView: AnyView(Text("Please select a Remote Type in Settings.")))
-            }
-
-        // Attach hosting controller if present
+            let remoteView = TrioNightscoutRemoteView()
+            hostingController = UIHostingController(rootView: AnyView(remoteView))
             if let hostingController = hostingController {
                 addChild(hostingController)
                 view.addSubview(hostingController.view)
-
                 hostingController.view.translatesAutoresizingMaskIntoConstraints = false
                 NSLayoutConstraint.activate([
                     hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -94,19 +62,61 @@ class RemoteViewController: UIViewController {
                     hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
                     hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
                 ])
-
                 hostingController.didMove(toParent: self)
             }
+        } else if remoteType == .trc {
+            let trioRemoteControlViewModel = TrioRemoteControlViewModel()
+            let trioRemoteControlView = TrioRemoteControlView(viewModel: trioRemoteControlViewModel)
+            hostingController = UIHostingController(rootView: AnyView(trioRemoteControlView))
+            if let hostingController = hostingController {
+                addChild(hostingController)
+                view.addSubview(hostingController.view)
+                hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+                    hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+                ])
+                hostingController.didMove(toParent: self)
+            }
+        } else if remoteType == .sms {
+            // Load SMSRemoteViewController
+            let storyboard = UIStoryboard(name: "Main", bundle: nil) // Replace "Main" with your storyboard name
+            guard let smsRemoteVC = storyboard.instantiateViewController(withIdentifier: "SMSRemoteViewController") as? SMSRemoteViewController else {
+                print("Error: SMSRemoteViewController could not be instantiated.")
+                return
+            }
 
-        if remoteType == .nightscout, !ObservableUserDefaults.shared.nsWriteAuth.value {
-            NightscoutUtils.verifyURLAndToken { error, jwtToken, nsWriteAuth in
-                DispatchQueue.main.async {
-                    ObservableUserDefaults.shared.nsWriteAuth.value = nsWriteAuth
-                }
+            // Add as child view controller
+            addChild(smsRemoteVC)
+            view.addSubview(smsRemoteVC.view)
+            smsRemoteVC.view.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                smsRemoteVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                smsRemoteVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                smsRemoteVC.view.topAnchor.constraint(equalTo: view.topAnchor),
+                smsRemoteVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+            smsRemoteVC.didMove(toParent: self)
+        } else {
+            // Default case
+            hostingController = UIHostingController(rootView: AnyView(Text("Please select a Remote Type in Settings.")))
+            if let hostingController = hostingController {
+                addChild(hostingController)
+                view.addSubview(hostingController.view)
+                hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                    hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                    hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+                    hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+                ])
+                hostingController.didMove(toParent: self)
             }
         }
     }
-
+    
     deinit {
         cancellable?.cancel()
     }
