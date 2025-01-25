27efcf3ddcee011991dfb21490c16e7f1a269148
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2025-01-25 19:09:32 +0100

    Björkerts commi: Share todays and yesterdays log file.

--- LoopFollow/Controllers/Nightscout/DeviceStatusOpenAPS.swift
+++ LoopFollow/Controllers/Nightscout/DeviceStatusOpenAPS.swift
@@ -41,7 +41,7 @@ extension MainViewController {
                     let formattedLastLoopTime = dateFormatter.string(from: Date(timeIntervalSince1970: lastLoopTime))
                     
                     // Log the formatted time
-                    LogManager.shared.log(category: .deviceStatus, message: "New LastLoopTime: \(lastLoopTime)", isDebug: true)
+                    LogManager.shared.log(category: .deviceStatus, message: "New LastLoopTime: \(formattedLastLoopTime)", isDebug: true)
                     
                     //evaluateNotLooping(lastLoopTime: UserDefaultsRepository.alertLastLoopTime.value)
                 } else {
