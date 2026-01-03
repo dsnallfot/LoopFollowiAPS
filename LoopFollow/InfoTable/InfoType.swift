570862feb38581eb5c34b572d35ef372fda38c37
dsnallfot <72826201+dsnallfot@users.noreply.github.com>
2026-01-03 16:58:23 +0100

    Lagt till btPing case för infotable..

--- LoopFollow/InfoTable/InfoType.swift
+++ LoopFollow/InfoTable/InfoType.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum InfoType: Int, CaseIterable {
-    case iob, cob, basal, override, battery, pump, sage, cage, recBolus, minMax, carbsToday, autosens, profile, target, isf, carbRatio, updated, tdd, iage, carbReq, af, smbRatio, pumpStatus, smbStatus, SMBUAMmin, autosensMinMax, maxSMB, overridePercentage, bgi, dev, totIob
+    case iob, cob, basal, override, battery, pump, sage, cage, recBolus, minMax, carbsToday, autosens, profile, target, isf, carbRatio, updated, tdd, iage, carbReq, af, smbRatio, pumpStatus, smbStatus, SMBUAMmin, autosensMinMax, maxSMB, overridePercentage, bgi, dev, totIob, btPing
 
     var name: String {
         switch self {
@@ -44,6 +44,7 @@ enum InfoType: Int, CaseIterable {
         case .bgi: return "BGI (5m)"
         case .dev: return "Dev (30m)"
         case .totIob: return "IOB + Basal IOB"
+        case .btPing: return "BLE heartbeat"
         }
     }
 
